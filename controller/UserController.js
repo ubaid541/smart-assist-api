82c6431d3cfd075360a9654cf24ff5e262adf461
ubaid541 <malikunknown633@gmail.com>
2023-02-15 09:04:56 +0500

    Empty field validations

--- controller/UserController.js
+++ controller/UserController.js
@@ -6,6 +6,12 @@ import jwt from "jsonwebtoken"
 const UserController = {
     async register(req,res,next){
 
+        const { name, email } = req.body;
+
+        if (!name || !email) {
+          return res.status(409).json( 'Name or email field is empty');
+        }
+
         //Check if username,email exists
         try {
             const exists = await User.exists({$or:[{email:req.body.email},{username:req.body.username}]})
@@ -29,12 +35,14 @@ const UserController = {
            const new_user = await newUser.save()
            res.status(200).json(["Successfully registered.",new_user])
         } catch (error) {
+            console.log(error);
             next(error)
         }
     },
     async login(req,res,next){
+  
         try {
-            const user = await User.findOne({name: req.body.name})
+            const user = await User.findOne({email: req.body.email})
 
             if(!user){
                 return next(createError(404,"User not found!"))
@@ -58,6 +66,7 @@ const UserController = {
             }).status(200).send({details:{...otherDetails},role})
 
         } catch (error) {
+            console.log(error);
             next(error)
         }
     },
